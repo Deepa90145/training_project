b45036937c30333ee0aa5654e5d06814f2694068
deepa <deepa.k@vvdntech.in>
2020-07-29 18:41:11 +0530

    prefix and postfix operator

---
+++ c++/21julytask5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	unsigned int i=5,j,k;
+	j=i++;
+        k=i--;
+        cout<<"Increment of i:"<<" "<<j<<" "<<"decrement of i:"<<" "<<k<< endl;
+
+	return 0;
+
+	}
+
