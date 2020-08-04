fbf3f85433f85a8ce727a7707a8d962fa3720a87
deepa <deepa.k@vvdntech.in>
2020-08-04 18:37:36 +0530

    Applying use cases to directory and file

---
+++ shell_scripting/filedir_usecase.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+echo "Enter dirctory name and file name"
+read ndir nfile
+
+
+#write in file
+WriteFile()
+{   
+        echo "Press 'W' to write  content into the file and 'N' for no"
+        read var
+        case $var in
+        'W')
+        echo "Enter content"
+        read content
+        echo $content >> $nfile
+        echo "Content in the file is:"
+        cat $nfile
+        ;;
+        
+        'N')
+            echo "thank you"
+      ;;
+        esac 
+        
+}
+
+
+
+#function to create a new file
+CreateFile()
+{
+         touch $nfile
+         echo "file created successfully"
+}
+
+
+
+
+#Function to check whether file already exist or not
+CheckFile()
+{   
+         if [ -f "$nfile" ]
+         then
+         {
+          echo "File exist"
+          WriteFile
+         }
+          else
+           {
+         echo "file not exist"
+         CreateFile
+         WriteFile
+       }
+                   fi
+}
+
+
+
+if [ -d "$ndir" ]
+then
+{
+        echo "Directory exist"
+         cd $ndir
+         CheckFile
+}
+else
+{
+        echo "Directory does not exist"
+        mkdir $ndir
+        echo "New directory created"
+        cd $ndir
+        CheckFile
+                                
+}
+fi
