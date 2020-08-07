52c6b88fbad37366aba1a4434509c8a31e3d1a15
deepa <deepa.k@vvdntech.in>
2020-08-07 15:45:23 +0530

    Second level and third level json

---
+++ c_programming/secondlevel_json.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "cJSON.h"
+#include <string.h>
+
+void *create_employee();
+int main() {
+    create_employee();
+
+    return 0;
+
+
+}
+
+void *create_employee(){
+
+    char *string = NULL;
+
+    //creating json object
+    cJSON *employee = cJSON_CreateObject();
+    //checking for exception    
+    if (employee == NULL)
+    {
+        goto end;
+    }
+
+    //creating another json object
+    cJSON *employee_detail=cJSON_CreateObject();
+    //checking for expection
+    if (employee_detail == NULL)
+    {
+        goto end;
+    }
+    //passing string to first object "employee"
+    cJSON_AddStringToObject(employee,"Employee:","101");
+
+    //adding values to second object "employee_details"
+    cJSON_AddStringToObject(employee_detail, "NAME:","Deepa");
+    cJSON_AddNumberToObject(employee_detail,"Age",20);
+    cJSON_AddNumberToObject(employee_detail,"ID",101);
+    cJSON_AddBoolToObject(employee_detail,"Boolean value",1);
+
+
+    // creating third json object
+    cJSON *personal_details=cJSON_CreateObject();
+
+    //checking for expection
+    if (personal_details== NULL)
+    {
+        goto end;
+    }
+    //passing second object "employee_details" to third object "personal_details"
+    cJSON_AddItemToObjectCS(personal_details,"personal_details",employee_detail);
+
+    //passing third object to main object "employee"
+    cJSON_AddItemToObjectCS(employee,"employee_detail:",personal_details);
+
+
+    string = cJSON_Print(employee);
+    if (string == NULL)
+    {
+        fprintf(stderr, "Failed to print monitor.\n");
+    }
+
+end:
+    cJSON_Delete(employee);
+    printf("%s",string);
+
+
+}
